3a717715bfa10725a60ce9321f5af0717f854ac7
Vurukuti Govinda Raju Yadav <vgovindev@gmail.com>
2021-04-10 18:48:29 +0530

    Adding swagger

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/User/User.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/User/User.java
@@ -5,9 +5,12 @@ import java.util.Date;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
+import io.swagger.annotations.ApiModel;
+
+@ApiModel(description = "All details about the user")
 public class User {
 	private Integer id;
-	@Size(min = 2, message="Name should have atleast two characters")
+	@Size(min = 2, message = "Name should have atleast two characters")
 	private String name;
 	@Past
 	private Date birthDate;
