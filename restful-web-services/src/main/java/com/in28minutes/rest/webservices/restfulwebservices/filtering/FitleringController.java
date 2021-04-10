d4e685884ca9a9e605a96810aeae71dbe12259d3
Vurukuti Govinda Raju Yadav <vgovindev@gmail.com>
2021-04-10 21:10:17 +0530

    Static filtering using JsonIgnorePropery and JsonIgnoreProperties

---
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/filtering/FitleringController.java
@@ -0,0 +1,20 @@
+package com.in28minutes.rest.webservices.restfulwebservices.filtering;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FitleringController {
+	@GetMapping("/filtering")
+	public SomeBean retrieveSomeBean() {
+		return new SomeBean("value1", "value2", "value3");
+	}
+
+	@GetMapping("/filtering-list")
+	public List<SomeBean> retrieveSomeBeans() {
+		return Arrays.asList(new SomeBean("value1", "value2", "value3"), new SomeBean("value11", "value22", "value33"));
+	}
+}
