ef7059e5bdc4e14070a7a47bd3ce900d0c1de388
Vurukuti Govinda Raju Yadav <vgovindev@gmail.com>
2021-04-10 17:12:44 +0530

    Validations using spring validation api

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/User/User.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/User/User.java
@@ -2,9 +2,14 @@ package com.in28minutes.rest.webservices.restfulwebservices.User;
 
 import java.util.Date;
 
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+
 public class User {
 	private Integer id;
+	@Size(min = 2, message="Name should have atleast two characters")
 	private String name;
+	@Past
 	private Date birthDate;
 
 	public Integer getId() {
--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/exception/CustomizedResponseEntityExceptionHandler.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/exception/CustomizedResponseEntityExceptionHandler.java
@@ -2,8 +2,10 @@ package com.in28minutes.rest.webservices.restfulwebservices.exception;
 
 import java.util.Date;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,5 +30,14 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(),
 				request.getDescription(false));
 		return new ResponseEntity(exceptionResponse, HttpStatus.NOT_FOUND);
+	}
+
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		// String message = ex.getBindingResult().toString();
+		String message = "Validation Failed";
+		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), message, ex.getMessage());
+		return new ResponseEntity(exceptionResponse, HttpStatus.BAD_REQUEST);
 	}
 }
